b70281f332223b8bb8ade38a3352e2170d6530cb
AhmedAmr <AhmedAmron@hotmail.com>
2015-12-30 11:54:06 +0300

    Solve Codeforces  A. Team greedy,bruteforce

---
+++ A. Team/src/Main.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/**
+ * Created by AhmedAmr on 12/30/15.
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<Integer> line = readInts(nextLine(in));
+        int count = 0;
+        int n = line.get(0);
+        for (int i = 0; i < n; i++) {
+            line = readInts(nextLine(in));
+            int temp = 0;
+            for (Integer integer : line) {
+                temp+=integer ;
+            }
+            if(temp>1)count++;
+        }
+        System.out.println(count);
+    }
+
+    public static ArrayList<Integer> readInts(String line) {
+        String[] arr = line.split(" ");
+        ArrayList<Integer> res = new ArrayList<>();
+        for (String s : arr) {
+            if (!s.isEmpty()) res.add(Integer.parseInt(s));
+        }
+        return res;
+    }
+
+
+
+    public static String nextLine(BufferedReader in) throws IOException {
+        String line = in.readLine();
+        while (line.isEmpty()) {
+            line = in.readLine();
+        }
+        return line;
+    }
+
+}
